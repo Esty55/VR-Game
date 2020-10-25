4b1a81e2a2b27deb0f8cc81490300c155b0fc33f
Esty <36269009+rafidul@users.noreply.github.com>
2020-10-25 20:35:44 +0600

    Scripts

---
+++ Assets/VR/Scripts/LifeControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeControl : MonoBehaviour {
+
+    public AudioClip Fall; //fall sound of eagle
+    public ParticleSystem SplashEffect;
+    public AudioClip WaterSplash;
+    private int cap;
+
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Terrain")) //all collided objects needs to be set Terrain tag
+        {
+            Camera.main.GetComponent<GameLogic>().impacted = true;
+            Camera.main.GetComponent<AudioSource>().PlayOneShot(Fall, 0.7F);
+            StartCoroutine(Camera.main.GetComponent<GameLogic>().ResetByFall(0.2F,0));
+        }
+    }
+
+}
